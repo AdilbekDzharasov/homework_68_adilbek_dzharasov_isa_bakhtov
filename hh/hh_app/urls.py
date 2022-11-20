d8972e895e64d5760b432868c8ebab585c6a5050
Isa Bakhtov <issab79@icloud.com>
2022-11-20 13:15:30 +0600

    #17 Создан функционал отображения списка резюме: создана вьюха, шаблон для веб страницы и путь зарегистрирован в urls.py как resume/list/

--- hh/hh_app/urls.py
+++ hh/hh_app/urls.py
@@ -4,6 +4,7 @@ from hh_app.views.vacancy_detail import VacancyDetailView
 from hh_app.views.vacancy_home import HomeVacancyView
 from hh_app.views.vacancy_update import VacancyUpdateView, update_date, VacancyPublicUpdateView
 from hh_app.views.resume_add import ResumeAddView
+from hh_app.views.resume_home import HomeResumeView
 
 urlpatterns = [
     path('', HomeVacancyView.as_view(), name='home'),
@@ -14,5 +15,6 @@ urlpatterns = [
     path('vacancy/update/public/<int:pk>', VacancyPublicUpdateView.as_view(), name='vacancy_update_public'),
     
     path('resume/add', ResumeAddView.as_view(), name='resume_add'),
+    path('resume/list/', HomeResumeView.as_view(), name='resume_home'),
     ]
 
---
+++ hh/hh_app/views/resume_home.py
@@ -0,0 +1,40 @@
+from django.views.generic import ListView
+from django.db.models import Q
+from django.utils.http import urlencode
+from hh_app.models import Resume
+from hh_app.forms import ResumeForm
+
+
+class HomeResumeView(ListView):
+    template_name = 'resume/resume_home.html'
+    context_object_name = 'resumes'
+    model = Resume
+    ordering = ('-created_at')
+
+    def get(self, request, *args, **kwargs):
+        self.form = self.get_search_form()
+        self.search_value = self.get_search_value()
+        return super().get(request, *args, **kwargs)
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(HomeResumeView, self).get_context_data(object_list=object_list, **kwargs)
+        context['form'] = self.form
+        if self.search_value:
+            context['query'] = urlencode({'search': self.search_value})
+        return context
+
+    def get_queryset(self):
+        queryset = super().get_queryset().exclude(is_public=False)
+        if self.search_value:
+            query = Q(author__icontains=self.search_value) | Q(education__icontains=self.search_value)
+            queryset = queryset.filter(query)
+        return queryset
+
+    def get_search_form(self):
+        return ResumeForm(self.request.GET)
+
+    def get_search_value(self):
+        if self.form.is_valid():
+            return self.form.cleaned_data.get('search')
+        return None
+
