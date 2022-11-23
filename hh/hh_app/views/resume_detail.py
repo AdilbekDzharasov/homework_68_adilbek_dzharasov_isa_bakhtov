71e7243d015aa5ff4b52dac82c797f9df9c58804
Isa Bakhtov <issab79@icloud.com>
2022-11-23 20:07:46 +0600

    #30 Доработали функционал вывода информации резюме. Для этого внесли изменения во вьюху и шаблон (I-17)

--- hh/hh_app/views/resume_detail.py
+++ hh/hh_app/views/resume_detail.py
@@ -1,19 +1,23 @@
 from django.views.generic import DetailView
-from hh_app.models import Resume
+from django.shortcuts import get_object_or_404
+
+from hh_app.models import Resume, Education, Experience
 
 
 class ResumeDetailView(DetailView):
     template_name = "resume/resume_detail.html"
     context_object_name = 'resume'
     model = Resume
+    pk_url_kwarg = 'pk'
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super(ResumeDetailView, self).get_context_data(object_list=object_list, **kwargs)
         resume = self.object
-        educations = resume.education
+        educations = Education.objects.filter(resume_id = self.kwargs['pk'])
         context['educations'] = educations
-        experiences = resume.experience
+        experiences = Experience.objects.filter(resume_id = self.kwargs['pk'])
         context['experiences'] = experiences
+        
         return context
 
 
